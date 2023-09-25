651f0463e9ca79f1f4bbe5c91fddf744481fd060
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-25 13:05:18 +0000

    added routes

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/api/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    if not user.name:
+        raise HTTPException(status_code=400, detail="Name is required")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/api/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+    
+@app.put("/api/{user_id}", response_model=schemas.User)
+def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.update_user(db=db, user_id=user_id, user=user)
+    
+
+@app.delete("/api/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.delete_user(db=db, user_id=user_id)
